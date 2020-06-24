4ae8fa5be3867268478fea90772f00f217ab5c39
Brent Irwin <brent@brentirwin.tv>
2020-06-24 12:12:12 -0500

    Initial Commit

---
+++ SunshineIndex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SunshineIndex
+//
+//  Created by Brent Irwin on 6/24/20.
+//  Copyright © 2020 Brent Irwin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
